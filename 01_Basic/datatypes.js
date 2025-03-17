a1063d7bcb2ace1fc54e598ce43a22c7c43567db
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-17 19:57:15 +0000

    done

---
+++ 01_Basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all code as newer version
+
+// alert("Hello")
+//we are use node.js not browser
+
+console.log(3+3);// code redablity high
+
+console.log("Pratham");
+
+//number = 2 to power 53
+//bigint 
+//boolean true/false
+//string ""
+//null==> standalone value say empty
+//undefine =>not assign after make variable
+//symbol ==> unique
+
+//object
+
+console.log(typeof null);//op is object
+console.log(typeof undefined);//undefine
+
+
---
+++ 01_Basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 14453
+let accountemail="prathammodi17@gmail.com"
+var accountPassward ="12345"
+accountCity="Gandhinagar"
+
+// accountId = 876
+console.log(accountId);
+
+accountemail= "pm12@gmail.com"
+
+accountPassward="2111233"
+accountCity="jaiour"/*
+*/
+let accountState;
+
+//not prefer to use var keyword due to body and fuction bracket problem
+
+console.table([accountId,accountemail, accountPassward,accountCity,accountState])
