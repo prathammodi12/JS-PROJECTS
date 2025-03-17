d894b6f2fa234928500cdc5fe501fd4bf53373f0
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-17 20:10:59 +0000

    operation

---
+++ 01_Basic/conversionOpration.js
@@ -0,0 +1,22 @@
+// let score = "33abc"
+// let score = null
+let score = undefined
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*"33"==>33
+"33abc"==>NaN
+true==> 1 , false==>0
+""==> false
+"Pratham"==> true
+*/
+let islogin
+
+let booleanislogin=Boolean(islogin)
+console.log(booleanislogin);
+
