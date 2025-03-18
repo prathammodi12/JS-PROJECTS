05add8111b9fd04dd6d44dbacab1015a8ee1ff3b
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-18 05:13:50 +0000

    done

--- 01_Basic/conversionOpration.js
+++ 01_Basic/conversionOpration.js
@@ -18,5 +18,35 @@ true==> 1 , false==>0
 let islogin
 
 let booleanislogin=Boolean(islogin)
-console.log(booleanislogin);
+// console.log(booleanislogin);
+
+let value = 3
+let negValue = -value 
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2**3);
+
+let str1= "Pratham"
+let str2 = " Modi"
+
+let str3= str1 + str2 
+console.log(str3);
+console.log(1+ "2");
+console.log("1"+"2")
+
+console.log("1"+2 + 2);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2 , num3 
+
+num1=num2=num3= 2+2
+
+console.log(num2);
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
 
