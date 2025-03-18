26813efa67dc50c13455c7da8c9417e76a7ea5d5
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-18 11:44:31 +0000

    done

---
+++ 01_Basic/string.js
@@ -0,0 +1,23 @@
+const name= "Pratham"
+const repocount = 90
+
+// console.log(name + repocount + "Value");
+
+console.log(`Hello  my name is ${name } and my repo count is ${repocount}`);
+
+const gameName= new String('Pratham')  
+
+console.log(gameName[0]);
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+const newstring = gameName.substring(0,6) 
+console.log(newstring);
+const newstring1 = gameName.slice(-6,-2)
+console.log(newstring1);
+
+const newword="          Pratham.       "   
