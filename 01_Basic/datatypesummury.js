b3684e7b806f17a94f73aeea7f79c905df99f7f9
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-18 10:58:58 +0000

    done

--- 01_Basic/datatypesummury.js
+++ 01_Basic/datatypesummury.js
@@ -35,3 +35,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// stack (primitive), heap(Non-primitive)
+
+let ytname="Pratham"
+
+let another=ytname
+another="Chaiaurcode"
+
+console.log(ytname);
+console.log(another);
+
+let user = {
+    email:"prathammodi@google.com",
+    upi:"user@ybl"
+
+}
+let user2= user
+user2.email="pratham@com"
+console.log(user.email);
+console.log(user2.email);
+
+
