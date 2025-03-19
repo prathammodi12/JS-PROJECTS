ca7b8b52249f4d2771c431682571d57a5f3313cc
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-19 17:48:33 +0000

    done

---
+++ 01_Basic/date_time.js
@@ -0,0 +1,9 @@
+// Dateb 
+ 
+let myDate =new Date()
+
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+
+console.log(typeof myDate);
+console.log(Date.now());
---
+++ 01_Basic/num_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString.length);
+console.log(balance.toFixed(1));
+
+const otherNumeber =2223.09888 
+
+console.log((otherNumeber.toPrecision(3)));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++MATHS++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(90));
+// console.log(Math.round(4.83));
+// console.log(Math.ceil(3.7));
+// console.log(Math.floor(3.7));
+// console.log(Math.min(3,7,2,6,9,0));
+// console.log(Math.max(3,7,1,5,8,9));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
