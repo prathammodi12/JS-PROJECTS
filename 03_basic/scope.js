e3ea6098071e0055699d82fc208dc85097692e64
Pratham Modi <158206004+prathammodi12@users.noreply.github.com>
2025-03-20 17:56:42 +0000

    done

---
+++ 03_basic/scope.js
@@ -0,0 +1,23 @@
+c = 900
+
+if(true){
+let a =400
+const b = 10
+var c =30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+console.log(addone(8))
+
+function addone(num){
+    return num+1
+}
+
+console.log(addtwo(8));
+
+const addtwo= function(num){
+    return num+2
+}
